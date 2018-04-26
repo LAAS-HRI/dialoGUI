31f5ced3bc421c1840fc0dddbd06f527c8625523
Guillaume Sarthou <gsarthou@laas.fr>
2018-04-26 15:53:25 +0200

    [UPDATE] resize window in launch file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "include/dialoGUI.h"
 #include "include/DarkStyle.h"
 #include <QApplication>
+#include <QSize>
 
 #include "ros/ros.h"
 #include <iostream>
@@ -15,10 +16,13 @@ int main(int argc, char *argv[])
 {
     std::string name = std::string(argv[1]);
     std::cout << "name " << name << std::endl;
+    char *p;
+    size_t width = strtol(argv[2], &p, 10);
+    size_t height = strtol(argv[3], &p, 10);
     std::vector<std::string> ids;
     std::vector<std::string> topics;
 
-    int nb_topic = argc - 4;
+    int nb_topic = argc - 6;
     if(nb_topic % 2 != 0)
     {
       std::cout << "nb topic must be pair" << std::endl;
@@ -28,16 +32,19 @@ int main(int argc, char *argv[])
       nb_topic = nb_topic/2;
 
     for(int i = 0; i < nb_topic; i++)
-      ids.push_back(std::string(argv[2 + i]));
+      ids.push_back(std::string(argv[4 + i]));
 
     for(int i = 0; i < nb_topic; i++)
-      topics.push_back(std::string(argv[2 + nb_topic + i]));
+      topics.push_back(std::string(argv[4 + nb_topic + i]));
 
     QApplication a(argc, argv);
     a.setStyle(new DarkStyle);
     dialoGUI w;
     w.setWindowTitle(QString::fromStdString(name));
 
+    QSize size(width,height);
+
+    w.resize(size);
     w.show();
 
     ros::init(argc, argv, "dialoGUI", ros::init_options::AnonymousName);
